f4ec03866f11c3a576c03c5b250c9f2ec6cd5b66
wissemhammoudi <hammoudiwissem81@gmail.com>
2025-12-14 23:34:01 +0100

    test

--- vanilles/app/product/[id]/page.tsx
+++ vanilles/app/product/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { Navbar } from "@/components/navbar"
 import { Footer } from "@/components/footer"
 import { ProductDetails } from "@/components/product-details"
-import { ProductRecommendations } from "@/components/product-recommendations"
 
 export default function ProductPage({ params }: { params: { id: string } }) {
   return (
@@ -9,7 +8,6 @@ export default function ProductPage({ params }: { params: { id: string } }) {
       <Navbar />
       <main className="min-h-screen py-20">
         <ProductDetails productId={params.id} />
-        <ProductRecommendations />
       </main>
       <Footer />
     </>
